62265c41fbe9268e6f5024655a537f76b3c40c8c
icruz3 <59888011+icruz3@users.noreply.github.com>
2020-03-05 10:22:12 -1000

    initial commit and push

--- HackwichSeven/ViewController.swift
+++ HackwichSeven/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+     @IBOutlet weak var textField: UITextField!
+     @IBOutlet weak var displayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //set disp to have no text
+        
+        self.displayLabel.text = ""
+        self.view.backgroundColor = UIColor.gray
     }
-
+    
+    @IBAction func buttonPressed(_ sender: Any)
+    {
+        let userInputText = textField.text
+        //display user text
+        self.displayLabel.text = userInputText
+    }
+    
 
 }
-
